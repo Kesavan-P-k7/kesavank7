79ce496e990212f3a4b1c09f173bf6dd82bc2699
Kesavan P <kesavan.pk71@gmail.com>
2026-01-20 18:40:54 +0530

    again changes in style js lineeweek

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ init_db()
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route("/")
 def home():
